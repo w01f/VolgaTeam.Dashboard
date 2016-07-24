373c17813f1b69a10c72568121c421487fb9f9ea
Alexey Krupnov <akrupnov@yandex.ru>
2016-07-24 12:03:07 +0300

    Utilities: PowerPoint launcher added

---
+++ Utilities/PowerPointLauncher/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace PowerPointLauncher
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Process.GetProcesses().Where(p => p.ProcessName.ToUpper().Contains("POWERPNT")).ToList().ForEach(p => p.Kill());
+			var executableName = Path.GetFileNameWithoutExtension(Process.GetCurrentProcess().MainModule.FileName);
+			var rootPath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+			foreach (var f in Directory.GetFiles(rootPath, String.Format("{0}.pot?", executableName)))
+			{
+				try
+				{
+					Process.Start(new ProcessStartInfo(f) { UseShellExecute = true });
+					break;
+				}
+				catch
+				{
+				}
+			}
+		}
+	}
+}
